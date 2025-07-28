e2fecf89bf68851b0767bb3d7c7a97ca0ab8eb29
baardl <bard.lind@gmail.com>
2025-07-28 10:24:17 +0200

    Re-enabling TrendId for DesigoSensorId

--- src/test/java/no/cantara/realestate/sensors/desigo/DesigoSensorIdTest.java
+++ src/test/java/no/cantara/realestate/sensors/desigo/DesigoSensorIdTest.java
@@ -2,10 +2,20 @@ package no.cantara.realestate.sensors.desigo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DesigoSensorIdTest {
+
+    @Test
+    void testConstructors() {
+        DesigoSensorId desigoSensorId =  new DesigoSensorId(null, "desigoId1", "propertyId1");
+        DesigoSensorId desigoSensorIdSame =  new DesigoSensorId("SensorId1","desigoId1", "propertyId1");
+        assertFalse(desigoSensorId.equals(desigoSensorIdSame));
+        desigoSensorId.setId("SensorId1");
+        assertTrue(desigoSensorId.equals(desigoSensorIdSame));
+        assertEquals(desigoSensorId.hashCode(), desigoSensorIdSame.hashCode());
+
+    }
 
     @Test
     void testEquals() {
