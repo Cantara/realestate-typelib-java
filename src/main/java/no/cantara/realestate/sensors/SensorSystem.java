da7943ab967b588ab9e9fb78121ec052dff2ded5
baardl <bard.lind@gmail.com>
2025-09-01 16:54:47 +0200

    Renamed to propper Disruptive

--- src/main/java/no/cantara/realestate/sensors/SensorSystem.java
+++ src/main/java/no/cantara/realestate/sensors/SensorSystem.java
@@ -1,5 +1,5 @@
 package no.cantara.realestate.sensors;
 
 public enum SensorSystem {
-    simulator, bacnet, tfm, metasys, desigo, ecostructure, distruptivetech, airthings, other, unknown;
+    simulator, bacnet, tfm, metasys, desigo, ecostructure, disruptivetech, airthings, other, unknown;
 }
